5b6fd937a7e5a457e3d68bd184eb1db6373ebedb
KaiqueBorgesdeOliveira <kaique.borges.1995@gmail.com>
2025-02-11 17:51:50 -0300

    feat: Create new class user and, connection db

---
+++ maquininha/src/main/java/com/pagueSe/maquininha/MaquininhaApplication.java
@@ -0,0 +1,13 @@
+package com.pagueSe.maquininha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaquininhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaquininhaApplication.class, args);
+	}
+
+}
---
+++ maquininha/src/test/java/com/pagueSe/maquininha/MaquininhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagueSe.maquininha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaquininhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
